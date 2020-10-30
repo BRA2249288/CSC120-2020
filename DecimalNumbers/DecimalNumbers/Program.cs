62c4bca36f528607dfa84d7823fc261831a455a3
BRA2249288 <73249820+BRA2249288@users.noreply.github.com>
2020-10-30 16:07:51 -0700

    Update Program.cs
    Update program

--- DecimalNumbers/DecimalNumbers/Program.cs
+++ DecimalNumbers/DecimalNumbers/Program.cs
@@ -1,37 +1,55 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DecimalNumbers
+namespace DecimalToOct
 {
     class Program
     {
-        static void Main(string[] args)
-        {  ///345790
-            var decimalNumber = 345790;
+     
+          
+       
+        public static void Main()
+        {
 
-            // Step one you need to each digit
-            // The first Step to Convert it to Value to Spring -> Character
-            // So that you can get to each 2 3 and 9 
+            // if 88 is base 130 be the result 
+            // if 2333 is base 4435 be the result
+            int n = 2333; //Create array to store each number so you have a place for the remainder
+            // array to store octal number
+            int[] octalNum = new int[100];//Set Decimal place to 100 //setup a counter for array
 
-            var characterArray = decimalNumber.ToString().ToCharArray();
-            var result = 0;
-            var maxpower = characterArray.Length;
-            var counter = 0; 
-            foreach (var item in characterArray)
-            {   
-                
-                var digit = int.Parse(item.ToString());
-                var power = Math.Pow(10, maxpower - ++counter);
-                result = result + (int)(digit * power);
-                
-                Console.WriteLine(item);
-                
+            // counter for octal number array
+            int i = 0;//setup a loop to go through the remainders and store them these are the R above
+            while (n != 0) //Is the Mod modifier getting the Remainder
+            {
 
+                // storing remainder in octal array % is the MOD so remainder of the number of 8 than divide by 8
+                octalNum[i] = n % 8; //Is the Mod modifier getting the Remainder
+                n = n / 8; // add the current number left over and divide it by 8 until 0 which stops loop
+                i++; //Add one to the counter to loop once
             }
-            Console.WriteLine(result);
+
+           
+            System.Console.WriteLine("Result:");
+            var result = 0; // store result.
+            for (int j = i - 1; j >= 0; j--) 
+ //perform loop until j is empty basically going backwards from length down to zero to output the numbers;
+            {
+                // Printing octal number array in reverse order
+                Console.Write(octalNum[j]);
+                //Concate int strings to get results 
+                result = int.Parse(result.ToString() + octalNum[j].ToString());
+// converts so can add result and add each digit of j so it’s not lost and stored in the result. 
+            }
+
+            Console.WriteLine("This result should match with top Result: " + result);
+            Console.ReadLine(); 
+
+               
+            
+
 
         }
     }
