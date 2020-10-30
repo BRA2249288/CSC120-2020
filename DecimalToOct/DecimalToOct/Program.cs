90c2a2eaf2a65a40eca273c51f3f04b10718217b
BRA2249288 <BRA2249288@maricopa.edu>
2020-10-30 16:05:03 -0700

    Updated Changes and Additional assignments attached.

--- DecimalToOct/DecimalToOct/Program.cs
+++ DecimalToOct/DecimalToOct/Program.cs
@@ -16,30 +16,32 @@ namespace DecimalToOct
 
             // if 88 is base 130 be the result 
             // if 2333 is base 4435 be the result
-            int n = 2333; 
+            int n = 2333; //Create array to store each number so you have a place for the remainder
             // array to store octal number
-            int[] octalNum = new int[100];
+            int[] octalNum = new int[100];//Set Decimal place to 100 //setup a counter for array
 
             // counter for octal number array
-            int i = 0;
-            while (n != 0)
+            int i = 0;//setup a loop to go through the remainders and store them these are the R above
+            while (n != 0) //Is the Mod modifier getting the Remainder
             {
 
                 // storing remainder in octal array % is the MOD so remainder of the number of 8 than divide by 8
-                octalNum[i] = n % 8;
-                n = n / 8;
-                i++;
+                octalNum[i] = n % 8; //Is the Mod modifier getting the Remainder
+                n = n / 8; // add the current number left over and divide it by 8 until 0 which stops loop
+                i++; //Add one to the counter to loop once
             }
 
            
             System.Console.WriteLine("Result:");
-            var result = 0; 
-            for (int j = i - 1; j >= 0; j--)
+            var result = 0; // store result.
+            for (int j = i - 1; j >= 0; j--) 
+ //perform loop until j is empty basically going backwards from length down to zero to output the numbers;
             {
                 // Printing octal number array in reverse order
                 Console.Write(octalNum[j]);
                 //Concate int strings to get results 
                 result = int.Parse(result.ToString() + octalNum[j].ToString());
+// converts so can add result and add each digit of j so it’s not lost and stored in the result. 
             }
 
             Console.WriteLine("This result should match with top Result: " + result);
