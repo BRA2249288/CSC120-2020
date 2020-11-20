29323c3d6654fb5424542265a78309ba11d3eedd
BRA2249288 <73249820+BRA2249288@users.noreply.github.com>
2020-11-20 07:05:59 -0700

    PartAandB

---
+++ Week3BooleanIdentity/PartA/Identity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartA
+{
+    class Identity
+    {
+        public bool SetInputD { get; set;  }
+
+        public bool SetInputA { get; set;  }
+
+        public bool SetInputX { get; set;  }
+
+        public bool Validate()
+        {
+            var result = true;
+            // you will need to add your solution here
+
+            
+            return result;
+
+        }
+    }
+}
