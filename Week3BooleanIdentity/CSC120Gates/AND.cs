791e183b5d1d9fc9b4b3ef6b38dff0001cddac66
BRA2249288 <73249820+BRA2249288@users.noreply.github.com>
2020-11-20 07:02:21 -0700

    Week3

---
+++ Week3BooleanIdentity/CSC120Gates/AND.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC120Gates
+{
+    public  class AND
+    {
+        //public bool InPutA { get; set; }
+        //public bool InPutB { get; set; }
+        //public AND(bool InPutA, bool InPutB)
+        //{
+        //    this.InPutA = InPutA;
+        //    this.InPutB = InPutB;
+        //}
+        //public AND()
+        //{
+            
+        //}
+        //public SetInputs(bool InPutA, bool InPutB)
+        //{
+        //    this.InPutA = InPutA;
+        //    this.InPutB = InPutB;
+
+        //}
+        //public bool OutPut()
+        //{
+        //    return InPutA && InPutB;
+        //}
+    }
+}
---
+++ Week3BooleanIdentity/CSC120Gates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC120Gates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // WriteThis(); 
+
+        }
+
+        public static void WriteThis(){
+            // Console.WriteLine("Test Data");
+
+        }
+    }
+}
