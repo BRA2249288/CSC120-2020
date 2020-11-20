04407e791e1459d02383841554a97b5f000cae8a
BRA2249288 <73249820+BRA2249288@users.noreply.github.com>
2020-11-20 07:07:09 -0700

    PartB

---
+++ Week3BooleanIdentity/PartB/Identity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartB
+{
+    class Identity
+    {
+        public bool SetInputA { get; set; }
+
+        public bool SetInputB { get; set; }
+
+        public bool Output { get; set; }
+
+        public bool Validate()
+        {
+            var result = true;
+
+            return result;
+
+        }
+    }
+}
---
+++ Week3BooleanIdentity/PartB/IndentityInput.cs
@@ -0,0 +1,12 @@
+﻿//using LogicCircuit.Gates.Simple;
+//using CSC120Gates;
+namespace PartB
+{
+    internal class IndentityInput
+    {
+
+        public bool A { get; set; }
+        public bool B { get; set; }
+
+    }
+}
