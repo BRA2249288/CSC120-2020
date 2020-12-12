5f4f72a670375080ee31c32b3a1fe5c4651166ca
BRA2249288 <73249820+BRA2249288@users.noreply.github.com>
2020-12-11 20:23:56 -0700

    Update to game
    Game Array

---
+++ TestGame/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace TestGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblHidden = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chkTimer = new System.Windows.Forms.CheckBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.countTimer = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.chkLabels = new System.Windows.Forms.CheckBox();
+            this.chkSound = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(33, 130);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(900, 308);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(283, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Disarray: An Array Game";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(360, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score =";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblScore.Location = new System.Drawing.Point(430, 42);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(24, 27);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "0";
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblQuestion.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblQuestion.Location = new System.Drawing.Point(308, 102);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(86, 27);
+            this.lblQuestion.TabIndex = 4;
+            this.lblQuestion.Text = "Question";
+            // 
+            // lblHidden
+            // 
+            this.lblHidden.AutoSize = true;
+            this.lblHidden.Location = new System.Drawing.Point(759, 102);
+            this.lblHidden.Name = "lblHidden";
+            this.lblHidden.Size = new System.Drawing.Size(108, 25);
+            this.lblHidden.TabIndex = 5;
+            this.lblHidden.Text = "hiddenLabel";
+            this.lblHidden.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 9000;
+            this.timer1.Tick += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // chkTimer
+            // 
+            this.chkTimer.AutoSize = true;
+            this.chkTimer.Location = new System.Drawing.Point(274, 70);
+            this.chkTimer.Name = "chkTimer";
+            this.chkTimer.Size = new System.Drawing.Size(82, 29);
+            this.chkTimer.TabIndex = 6;
+            this.chkTimer.Text = "Timer";
+            this.chkTimer.UseVisualStyleBackColor = true;
+            this.chkTimer.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTimer.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTimer.Location = new System.Drawing.Point(351, 70);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(34, 27);
+            this.lblTimer.TabIndex = 7;
+            this.lblTimer.Text = "60";
+            this.lblTimer.Visible = false;
+            // 
+            // countTimer
+            // 
+            this.countTimer.Interval = 1500;
+            this.countTimer.Tick += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(442, 473);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(112, 34);
+            this.btnRestart.TabIndex = 8;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // chkLabels
+            // 
+            this.chkLabels.AutoSize = true;
+            this.chkLabels.Location = new System.Drawing.Point(391, 70);
+            this.chkLabels.Name = "chkLabels";
+            this.chkLabels.Size = new System.Drawing.Size(87, 29);
+            this.chkLabels.TabIndex = 9;
+            this.chkLabels.Text = "Labels";
+            this.chkLabels.UseVisualStyleBackColor = true;
+            this.chkLabels.CheckedChanged += new System.EventHandler(this.chkLabels_CheckedChanged);
+            // 
+            // chkSound
+            // 
+            this.chkSound.AutoSize = true;
+            this.chkSound.Location = new System.Drawing.Point(475, 69);
+            this.chkSound.Name = "chkSound";
+            this.chkSound.Size = new System.Drawing.Size(90, 29);
+            this.chkSound.TabIndex = 10;
+            this.chkSound.Text = "Sound";
+            this.chkSound.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 557);
+            this.Controls.Add(this.chkSound);
+            this.Controls.Add(this.chkLabels);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.chkTimer);
+            this.Controls.Add(this.lblHidden);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblHidden;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox chkTimer;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer countTimer;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.CheckBox chkLabels;
+        private System.Windows.Forms.CheckBox chkSound;
+    }
+}
+
---
+++ TestGame/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Media;
+
+namespace TestGame
+{
+    public partial class Form1 : Form
+    {
+        int timeLeft = 60;
+        int doDisplay = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // int countThis = 0;
+            // string mainWord = "ELEPHANT!";
+            //"ELEPHANT".substring(0, 7);
+            bool FirstTimeRan = true;
+            FormLoadProcess(FirstTimeRan);
+
+            int returnNum = RandomQuestion();
+
+        }
+
+        private void FormLoadProcess(bool FirstTimeRan)
+        {
+
+
+            int controlCount = 8;
+
+            for (int i = 0; i < controlCount; i++)
+            {
+                var myButton = new Button();
+                var myLabel = new Label();
+                myLabel.Visible = false;
+                myButton.Click += ButtonClickHandler;
+                myButton.Height = 90;
+                myButton.Width = 90;
+
+
+                // mainWord.Substring(0, i);
+
+
+            
+
+                //  myLabel.Text = mainWord.Substring(countThis, i).ToString();
+                // countThis = countThis++; 
+                
+                myLabel.Text = i.ToString();
+
+                if (FirstTimeRan == true)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            myButton.BackColor = Color.Blue;
+                            myButton.ForeColor = Color.White;
+                            break;
+                        default:
+                            myButton.BackColor = Color.Blue;
+                            myButton.ForeColor = Color.White;
+                            break;
+                    }
+                    myButton.Text = i.ToString();
+                    this.flowLayoutPanel1.Controls.Add(myButton);
+                    this.flowLayoutPanel1.Controls.Add(myLabel);
+                }
+                else
+                {
+
+                    myButton.Text = "*";
+                    
+                }
+            }
+        }
+
+
+
+        private int RandomQuestion()
+        {
+            int min = 0;
+            int max = 8;
+
+            Random random = new Random();
+            int number = random.Next(min, max);
+            lblHidden.Text = number.ToString();
+
+            switch (number)
+            {
+
+                case 0:
+                    lblQuestion.Text = "System.out.print(a[0])";
+                    break;
+                case 1:
+                    lblQuestion.Text = "System.out.print(a[1])";
+                    break;
+                case 2:
+                    lblQuestion.Text = "System.out.print(a[2])";
+                    break;
+                case 3:
+                    lblQuestion.Text = "System.out.print(a[3])";
+                    break;
+                case 4:
+                    lblQuestion.Text = "System.out.print(a[4])";
+                    break;
+                case 5:
+                    lblQuestion.Text = "System.out.print(a[5])";
+                    break;
+                case 6:
+                    lblQuestion.Text = "System.out.print(a[6])";
+                    break;
+                case 7:
+                    lblQuestion.Text = "System.out.print(a[7])";
+                    break;
+                case 8:
+                    lblQuestion.Text = "System.out.print(a[8])";
+                    break;
+                case 9:
+                    lblQuestion.Text = "System.out.print(a[9])";
+                    break;
+            }
+
+            return number;
+        }
+
+        private void ButtonClickHandler(object sender, EventArgs e)
+        {
+            //var buttonThatWasClicked = sender as Button;
+            var buttonThatWasClicked = (Button)sender;
+            // var labelThatWasCLicked = (Label)sender; 
+
+            var intValue = int.Parse(buttonThatWasClicked.Text);
+            //  var intValue2 = buttonThatWasClicked; 
+            // MessageBox.Show(buttonThatWasClicked.Text);
+            var totalVal = 0;
+            var compareVal = int.Parse(lblHidden.Text);
+            if (compareVal == intValue)
+            {
+                totalVal = 0;
+            }
+            else
+            {
+                totalVal = 1;
+            }
+            int totalScore;
+
+            switch (totalVal)
+            {
+                case 0:
+                    buttonThatWasClicked.BackColor = Color.Green;
+                    buttonThatWasClicked.ForeColor = Color.White;
+
+                    totalScore = int.Parse(lblScore.Text) + 1;
+                    lblScore.Text = totalScore.ToString();
+
+                    break;
+                case 1:
+                    buttonThatWasClicked.BackColor = Color.Red;
+                    buttonThatWasClicked.ForeColor = Color.White;
+                    if (int.Parse(lblScore.Text) > 0)
+                    {
+                        totalScore = int.Parse(lblScore.Text) - 1;
+                        lblScore.Text = totalScore.ToString();
+
+                    }
+
+                    break;
+                default:
+                    buttonThatWasClicked.BackColor = Color.Blue;
+                    buttonThatWasClicked.ForeColor = Color.White;
+                    break;
+            }
+
+            if (buttonThatWasClicked.BackColor == Color.Green)
+            {
+                RandomQuestion();
+                MessageBox.Show("You Scored");
+                buttonThatWasClicked.BackColor = Color.Blue;
+                if (chkSound.Checked == true)
+                {
+                    SystemSounds.Beep.Play();
+                }
+            }
+
+            if (buttonThatWasClicked.BackColor == Color.Red)
+            {
+                RandomQuestion();
+                MessageBox.Show("You Were wrong");
+                buttonThatWasClicked.BackColor = Color.Blue;
+                if (chkSound.Checked == true)
+                {
+                    SystemSounds.Exclamation.Play();
+                }
+            }
+
+
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkTimer.Checked == true)
+            {
+
+                lblTimer.Visible = true;
+                timer1.Enabled = true;
+                countTimer.Enabled = true;
+                countTimer.Tick += new System.EventHandler(OnTimerCountEvent);
+                timer1.Tick += new System.EventHandler(OnTimerEvent);
+            }
+            else
+            {
+                timer1.Enabled = false;
+                countTimer.Enabled = false;
+                lblTimer.Visible = false;
+            }
+        }
+        private void OnTimerEvent(object sender, EventArgs e)
+        {
+            //  MessageBox.Show("You Lose");
+            //SystemSounds.Exclamation.Play();
+        }
+        private void OnTimerCountEvent(object sender, EventArgs e)
+        {
+
+            //lblTimer.Text
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Start();
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           
+
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                // Display time remaining as mm:ss
+                var timespan = TimeSpan.FromSeconds(timeLeft);
+                lblTimer.Text = timespan.ToString(@"ss");
+
+                // Alternate method
+                //int secondsLeft = timeLeft % 60;
+                //int minutesLeft = timeLeft / 60;
+            }
+            else
+            {
+                timer2.Stop();
+                if (chkSound.Checked == true)
+                {
+                    SystemSounds.Exclamation.Play();
+                }
+               
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                countTimer.Enabled = false;
+
+                if (doDisplay == 0)
+                {
+                   
+                    MessageBox.Show("Time's up!", "Time has elapsed", MessageBoxButtons.OK);
+                    doDisplay = 1;
+                    if (int.Parse(lblScore.Text) > 0)
+                    {
+                        int totalScore; 
+                        totalScore = int.Parse(lblScore.Text) - 1;
+                        lblScore.Text = totalScore.ToString();
+
+                    }
+                    RandomQuestion();
+                    chkTimer.Checked = false; 
+                }
+                
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            RandomQuestion();
+        }
+
+        private void chkLabels_CheckedChanged(object sender, EventArgs e)
+        {
+            FormLoadProcess(false);
+        }
+    }
+}
