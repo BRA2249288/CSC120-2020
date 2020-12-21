27c17287cdbd8b9ff3363d985133c4d9e466bd75
BRA2249288 <73249820+BRA2249288@users.noreply.github.com>
2020-12-21 11:19:09 -0700

    Update TruthTable.cs
    Update code from thursday

--- TruthTable.cs
+++ TruthTable.cs
@@ -11,5 +11,12 @@ namespace SimpleCircuits
         public Boolean X { get; set; }
         public Boolean D { get; set; }
         public Boolean R { get; set; }
+        
+        //updated code 
+        //public Int32 I1 { get; set; }
+        //public Boolean V1 { get; set; }
+        //public Boolean V2 { get; set; }
+        //public Boolean D1 { get; set; }
+        //public Boolean R1 { get; set; }
     }
 }
